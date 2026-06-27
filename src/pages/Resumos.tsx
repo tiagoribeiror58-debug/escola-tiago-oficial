a288dd7bde06f833e3958bd4711b58887f1e7396
Tiago Ribeiro <tiagoribeiror58@gmail.com>
2026-06-27 13:27:36 -0300

    fix: corrige syntax error no JSX do Resumos

--- src/pages/Resumos.tsx
+++ src/pages/Resumos.tsx
@@ -283,7 +283,6 @@ export default function Resumos() {
               ))}
             </div>
 
-            </div>
           </div>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-stretch">
